ed2567bbfd1f3142a09acfea8d6bf1085647af7e
Julien <cefadrom1@gmail.com>
2020-01-11 15:51:48 +0100

    Few changes and optimizations on DB type

--- web/public/script.js
+++ web/public/script.js
@@ -4,7 +4,10 @@ let _settings = {
 
 // Manage settings section visibility
 document.getElementById('toggle-settings').addEventListener('click', toggleSettings);
-document.getElementById('settings-reset').addEventListener('click', toggleSettings);
+document.getElementById('settings-reset').addEventListener('click', () => {
+    toggleSettings();
+    changeDbType(_settings.dbType);
+});
 
 function toggleSettings() {
     document.getElementById('settings').classList.toggle('collapse');
@@ -13,18 +16,19 @@ function toggleSettings() {
 
 // Manage the DB type selection
 const _settingsDbType = document.getElementById('settings-database-type');
-_settingsDbType.value = _settings.dbType;
-_settingsDbType.parentElement.after(...generateDbSettings(_settings.dbType));
-_settingsDbType.addEventListener('change', function () {
+changeDbType(_settings.dbType); // Set the DB type according to the settings
+_settingsDbType.addEventListener('change', () => {
+    changeDbType();
+});
+
+function changeDbType(type) {
+    type = type || _settingsDbType.options[_settingsDbType.selectedIndex].value || 'mysql';
+    _settingsDbType.value = type;
     document.querySelectorAll('.settings-database-specific').forEach(element => {
         element.remove();
     });
-    const selection = this.options[this.selectedIndex].value;
-    if (selection === 'mysql')
-        this.parentElement.after(...generateDbSettings('mysql'));
-    else if (selection === 'mongo')
-        this.parentElement.after(...generateDbSettings('mongo'));
-});
+    _settingsDbType.parentElement.after(...generateDbSettings(type));
+}
 
 function generateDbSettings(type) {
     let result;
@@ -39,11 +43,17 @@ function generateDbSettings(type) {
         <label class="d-block settings-database-specific">
             Password: <input type="text" class="form-control" id="settings-database-password">
         </label>
+        <label class="d-block settings-database-specific">
+            Database name: <input type="text" class="form-control" id="settings-database-name" required>
+        </label>
         `;
     else if (type === 'mongo')
         result = `
         <label class="d-block settings-database-specific">
             Uri: <input type="text" class="form-control" id="settings-database-uri" required>
+        </label>
+        <label class="d-block settings-database-specific">
+            Database name: <input type="text" class="form-control" id="settings-database-name" required>
         </label>
         `;
     // Convert the text to HTML childNodes
